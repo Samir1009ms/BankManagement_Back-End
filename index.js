288f4ff183e06969d037d0f6b2bdc53b5edc406f
Samir1009ms <yusifovs1009@gmail.com>
2023-04-17 05:53:08 +0400

    back-end

--- index.js
+++ index.js
@@ -18,19 +18,21 @@ app.use(cors());
 // * json
 app.use(express.json());
 
+const whitelist = ["*"]
+
 // * urlencoded
 app.use(express.json({limit: '50mb'}));
 
 app.use(express.urlencoded({limit: '50mb', extended: true,parameterLimit:50000}));
 
 // * login register post get
-app.use('/', Auth);
+app.use('/api', Auth);
 
 // * get post patch delete 
-app.use('/', Post);
+app.use('/api', Post);
 
 // * get post patch delete
-app.use('/', Cart);
+app.use('/api', Cart);
 
 
 app.get('/', (req, res) => {
